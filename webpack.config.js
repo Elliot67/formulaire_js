8c1fa4a8eb9010f96410eaea256252b3ab9ad608
Elliot67 <elliot.lintz@gmail.com>
2020-05-11 14:37:10 +0200

    [ADD] Ajout dev serveur hot reload webpakc

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
@@ -11,13 +12,18 @@ module.exports = env => {
     let filename;
     let scssModules;
     let plugins;
+    let devServer;
 
     if (env.MODE == "development") {
         devtool = "cheap-module-eval-source-map";
         mode = "development";
         watch = true;
         filename = "main.js";
-        plugins;
+        plugins = [
+            new HtmlWebpackPlugin({ // Pour ajouter un autre fichier HTML, simplement créer une nouvelle instance de l'objet - https://dev.to/mariorodeghiero/multiple-html-files-with-htmlwebpackplugin-19bf
+                template: "./src/template.html"
+            })
+        ];
         scssModules = [
             "style-loader", // Inject style js in the DOM
             { // Turns css to js
@@ -41,16 +47,58 @@ module.exports = env => {
         ];
     }
 
+    if (env.MODE == 'dev') {
+        devtool = "cheap-module-eval-source-map";
+        mode = "development";
+        watch = true;
+        filename = "main.js";
+        plugins = [
+            new HtmlWebpackPlugin({
+                template: "./src/template.html"
+            }),
+        ];
+        scssModules = [
+            "style-loader", // Inject style js in the DOM
+            { // Turns css to js
+                loader: 'css-loader',
+                options: {
+                    sourceMap: true
+                }
+            },
+            { // Ajouter des prefixes avec autoprefixer (fichier postcss.config.js)
+                loader: 'postcss-loader',
+                options: {
+                    sourceMap: true
+                }
+            },
+            { // Turns sass into css
+                loader: 'sass-loader',
+                options: {
+                    sourceMap: true
+                }
+            }
+        ];
+        devServer = {
+            contentBase: path.join(__dirname, 'dist'),
+            compress: true,
+            port: 8080,
+            allowedHosts: [],
+        };
+    }
+
     if (env.MODE == 'production') {
         devtool = false;
         mode = "production";
         watch = false;
-        filename = "main.js";
+        filename = "main-[contentHash].js";
         plugins = [
+            new HtmlWebpackPlugin({
+                template: "./src/template.html"
+            }),
             new MiniCssExtractPlugin({
                 filename: "style.css"
             }),
-            new CleanWebpackPlugin()
+            new CleanWebpackPlugin(),
         ];
         scssModules = [
             MiniCssExtractPlugin.loader, // Create CSS files
@@ -68,7 +116,7 @@ module.exports = env => {
         mode: mode,
         entry: {
             main: ["./src/index.js", "./src/index.scss"]
-            //vendor: "./src/vendor.js" Pour créer un fichier séparé consacré au librairies
+            //vendor: "./src/vendor.js" Pour créer un fichier séparé consacré aux librairies
         },
         devtool: devtool,
         watch: watch,
@@ -89,8 +137,14 @@ module.exports = env => {
                     test: /\.scss$/,
                     use: scssModules
                 },
-            ]
-        }
+                {
+                    test: /\.html$/,
+                    use: { // Pour importer les fichiers nécessaires
+                        loader: "html-loader",
+                    }
+                },
+            ],
+        },
+        devServer: devServer,
     }
-
 }
